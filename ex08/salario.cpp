b21d767adcce22b8a6054e033cc83e60ec9f77a1
Kaio Alves <kaio.alves19@gmail.com>
2024-08-20 11:38:44 -0300

    Teste de repositorio, contem 1 exercicio pronto (8)

--- ex08/salario.cpp
+++ ex08/salario.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
 
+int main(){
+    int NUMBER, horas;
+    double remuneracao ,salario;
+    cin >> NUMBER >> horas >> remuneracao;
 
+    salario = remuneracao * horas;
+
+    cout << fixed << setprecision(2);
+    cout << "NUMBER = " << NUMBER << endl;
+    cout << "SALARY = U$ " <<  salario << endl;
+
+    return 0;
+}
 
 
 
