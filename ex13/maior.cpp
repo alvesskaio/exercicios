214dfdea6fd9bf9d7303f9355d553ae9953a2e4a
Kaio Alves <kaio.alves19@gmail.com>
2024-08-20 23:20:02 -0300

    conclusão do ex 13

--- ex13/maior.cpp
+++ ex13/maior.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main(){
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    // Calcular o maior entre a e b
+    int maiorAB = (a + b + abs(a - b)) / 2;
+
+    // Calcular o maior entre maiorAB e c
+    int maior = (maiorAB + c + abs(maiorAB - c)) / 2;
+
+    cout << maior << " eh o maior" << endl;
 
     return 0;
 }
